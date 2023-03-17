1a517f23e2a84379274c4e8d760446b56b65a503
jmlee9707 <jmlee9707@naver.com>
2023-03-17 14:56:18 +0900

    fix : error 수정

--- src/components/dayRow/checkboxInput.jsx
+++ src/components/dayRow/checkboxInput.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const CheckboxInput = ({ name, onChange, checked, disabled }) => {
   return (
-    <ComponentWrapper>
+    <CheckboxInputArea>
       <Checkbox
         type="checkbox"
         name={name}
@@ -10,7 +10,6 @@ const CheckboxInput = ({ name, onChange, checked, disabled }) => {
         checked={checked}
         disabled={disabled}
       />
-      <Checkmark />
     </CheckboxInputArea>
   );
 };
@@ -42,5 +41,3 @@ const Checkbox = styled.input`
     background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='white' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M5.707 7.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L7 8.586 5.707 7.293z'/%3e%3c/svg%3e");
   }
 `;
-
-const Checkmark = styled.span``;
